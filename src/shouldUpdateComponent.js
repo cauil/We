adcbcb6a2d64ca9e80783b26d62e9e38b78e5d5e
cauil <xiaoloujun@gmail.com>
2019-02-13 19:15:44 +0800

    feat: add shouldUpdateComponent.js

---
+++ src/shouldUpdateComponent.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export default function shouldUpdateComponent(preEle, nextEle) {
+  const preType = typeof preEle;
+  const nextType = typeof nextEle;
+
+  if (preType === 'string') {
+    return nextType === 'string';
+  }
+
+  // In react we will also check the key;
+  return preEle.type === nextEle.type;
+}
